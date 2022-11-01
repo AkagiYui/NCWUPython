934d8057a27ac484428975c355882e65e2793a69
AkagiYui <ddio001@163.com>
2022-11-01 16:24:30 +0800

    fix: experiment 7 wrong

--- experiment/seventh/e2.py
+++ experiment/seventh/e2.py
@@ -1,4 +1,12 @@
-# 接着读取题目1中文件yzy.txt的内容，追加到文件yzy2.txt末尾
+"""
+2.	编写程序新建一个文本文件yzy2.txt，并写入下面两行内容：
+游子吟
+唐代：孟郊
+接着读取题目1中文件yzy.txt的内容，追加到文件yzy2.txt末尾，最后文件yzy2.txt的内容应该如下：
+"""
+
+with open('yzy2.txt', 'w', encoding='utf-8') as f:
+    f.writelines(['游子吟', '唐代：孟郊', '\n'])
 
 with open('yzy.txt', 'r', encoding='utf-8') as f:
     with open('yzy2.txt', 'a', encoding='utf-8') as f2:
