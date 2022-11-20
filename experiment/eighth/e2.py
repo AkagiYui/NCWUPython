511ffa404e2e25a25223c8c618292906097460ab
AkagiYui <ddio001@163.com>
2022-11-20 20:49:54 +0800

    fix: experiment 8 e2 e3

--- experiment/eighth/e2.py
+++ experiment/eighth/e2.py
@@ -5,18 +5,19 @@
 """
 
 import math
+import numpy as np
 import matplotlib.pyplot as plt
 
 # f(x)=sin(x)+x^2, x∈[0,2π]
-x = [i / 100 for i in range(0, 628)]  # 0, 0.01, 0.02, ..., 6.27
-y = [math.sin(i) + i ** 2 for i in x]
+x = np.arange(0, 2 * math.pi, 0.01)  # 0, 0.01, 0.02, ..., 6.27
+y = np.sin(x) + x ** 2  # 这是用numpy进行数组运算
 plt.title('f(x)=sin(x)+x^2, x∈[0,2π]')  # 设置标题
 plt.plot(x, y)
 plt.show()
 
 # f(x)=x^3+2*x^2+1, x∈[-2,2]
-x = [i / 100 for i in range(-200, 201)]  # -2, -1.99, -1.98, ..., 2
-y = [i ** 3 + 2 * i ** 2 + 1 for i in x]
+x = np.arange(-2, 2, 0.01)  # -2, -1.99, -1.98, ..., 2
+y = x ** 3 + 2 * x ** 2 + 1
 plt.title('f(x)=x^3+2*x^2+1, x∈[-2,2]')  # 设置标题
 plt.plot(x, y)
 plt.show()
--- experiment/eighth/e3.py
+++ experiment/eighth/e3.py
@@ -44,45 +44,50 @@ sheet_count = np.array([
 cost_each_product = sheet_cost.sum(axis=1)  # 每个产品的费用，每行求和
 count_each_product = sheet_count.sum(axis=1)  # 每个产品的数量
 total_cost = cost_each_product.dot(count_each_product)  # 总费用，矩阵乘法
-print(f'该工厂四个季度生产A、B、C三种产品所需的成本总和：{total_cost}')
+print(f'该工厂四个季度生产A、B、C三种产品所需的成本总和：{total_cost}万元')
 
 # 输出该工厂第一季度生产A、B、C三种产品所需的总成本。
 # 总成本 = 产品A第一季度数量*费用 + 产品B第一季度数量*费用 + 产品C第一季度数量*费用
 count_quarter = sheet_count[:, 0]  # 第一季度的数量，每列取第一个元素
 total_cost = count_quarter.dot(cost_each_product)
-print(f'该工厂第一季度生产A、B、C三种产品所需的总成本：{total_cost}')
+print(f'该工厂第一季度生产A、B、C三种产品所需的总成本：{total_cost}万元')
 
 # 分别输出该工厂每个季度生产所有产品的原料费总成本、人工费总成本、管理费总成本。
-# 第1季度 原料费总成本 人工费总成本 管理费总成本
-# 第2季度 原料费总成本 人工费总成本 管理费总成本
-# 第3季度 原料费总成本 人工费总成本 管理费总成本
-# 第4季度 原料费总成本 人工费总成本 管理费总成本
+# 第1季度：原料费总成本 人工费总成本 管理费总成本
+# 第2季度：原料费总成本 人工费总成本 管理费总成本
+# 第3季度：原料费总成本 人工费总成本 管理费总成本
+# 第4季度：原料费总成本 人工费总成本 管理费总成本
 data_table = sheet_cost.T.dot(sheet_count).T  # T: 转置
 for i, d in enumerate(data_table):
-    print(f"第{i + 1}季度 原料费总成本{d[0]} 人工费总成本{d[1]} 管理费总成本{d[2]}")
+    print(f"第{i + 1}季度 原料费总成本{d[0]}万元 人工费总成本{d[1]}万元 管理费总成本{d[2]}万元")
 
 # 把这些数据以二维数组的形式写入文件cost_product.csv中，元素之间用英文逗号分隔
 with open('cost_product.csv', 'w', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(data_table)
 
+# 绘图
 mpl.rc('font', family='SimHei', size=16)  # 设置字体，解决中文乱码问题
+plt.figure(figsize=(8, 12))  # 设置画布大小
+plt.subplots_adjust(hspace=0.5)  # 设置子图间距
+plt.suptitle('AkagiYui的matplotlib的绘图实验')  # 设置总标题
+
 # 绘制子图1，在该子图中绘制折线，显示四个季度A、B、C三种产品的产量变化趋势，
 # 横轴标签设置为“季度”，纵轴标签设置为“产量”。
-plt.figure(figsize=(8, 6))  # 设置画布大小
+plt.subplot(2, 1, 1)  # 占用2行1列，第1个子图
 plt.title('四个季度A、B、C三种产品的产量变化趋势')  # 设置标题
 plt.xlabel('季度')  # 设置横轴标签
-plt.ylabel('产量')  # 设置纵轴标签
+plt.ylabel('产量（件）')  # 设置纵轴标签
 plt.xticks(range(4), ['1', '2', '3', '4'])  # 设置横轴刻度
-plt.legend()  # 显示图例
 for i, d in enumerate(sheet_count):
-    plt.plot(d, label=f'产品{i + 1}')  # 绘制折线图
+    plt.plot(d, ['-.', '-', ':'][i], label=f'产品{i + 1}')  # 绘制折线图
     for ii, dd in enumerate(d):
         plt.text(ii, dd, dd, ha='center', va='bottom')  # 绘制折线图上的数据
-plt.show()
+plt.legend()  # 显示图例
 
-# 绘制子图2，在该子图中绘制饼图，显示三种产品的产量占比。
-plt.figure(figsize=(8, 6))
-plt.title('三种产品的产量占比')
-plt.pie(count_each_product, labels=['产品A', '产品B', '产品C'], autopct='%.2f%%')  # 绘制饼图
+# 绘制子图2，显示第一季度生产A、B、C三种产品所花费的原料成本、人工成本和管理费成本的比值，显示百分比（保留两位小数），
+# 饼图的标签对应显示“原料”、“人工”和“管理”。
+plt.subplot(2, 1, 2)  # 占用2行1列，第2个子图
+plt.title('第一季度生产三种产品所花费的\n原料成本、人工成本和管理费成本占比')
+plt.pie(data_table[0], labels=['原料', '人工', '管理'], autopct='%.2f%%')  # 绘制饼图
 plt.show()
